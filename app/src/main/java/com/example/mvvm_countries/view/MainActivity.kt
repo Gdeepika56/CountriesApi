79e9c7a9e904b468ba5c2b06da6aee993c5831fb
Gdeepika56 <dg1201@usnh.edu>
2025-07-07 15:12:14 -0400

    first commit

---
+++ app/src/main/java/com/example/mvvm_countries/view/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mvvm_countries.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.mvvm_countries.view.theme.MVVMCountriesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MVVMCountriesTheme {
+
+            }
+        }
+    }
+}
